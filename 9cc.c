8ed74eae0d00dd4f63f4455e18ac7e576bc95927
TomohiroKida <tomo426.sakura1281m@gmail.com>
2019-04-28 12:25:17 +0900

    トークナイザを導入

--- 9cc.c
+++ 9cc.c
@@ -1,5 +1,68 @@
+#include <ctype.h>
+#include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+// value representing type of token
+enum {
+  TK_NUM = 256, // integer token
+  TK_EOF,       // token representing end of input
+};
+
+typedef struct {
+  int ty;      // type of token
+  int val;     // if ty is 'TK_NUM', it is the value
+  char *input; // token string (for error message)
+} Token;
+
+// token array tokenized save this array.
+// define that more 100 tokens don't come
+Token tokens[100];
+ 
+// it report error
+// it have same arguments to 'printf'
+void error(char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+  vfprintf(stderr, fmt, ap);
+  fprintf(stderr, "\n");
+  exit(1);
+}
+
+// string that p arrowing are splited by token and saved to 'tokens'
+void tokenize(char *p) {
+  int i = 0;
+  while (*p) {
+    // skip space word
+    if (isspace(*p)) {
+      p++;
+      continue;
+    }
+
+    if (*p == '+' || *p == '-') {
+      tokens[i].ty    = *p;
+      tokens[i].input =  p;
+      i++;
+      p++;
+      continue;
+    }
+
+    if (isdigit(*p)) {
+      tokens[i].ty    = TK_NUM;
+      tokens[i].input = p;
+      tokens[i].val   = strtol(p, &p, 10);
+      i++;
+      continue;
+    }
+
+    error("don't tokenize: %s", p);
+    exit(1);
+  }
+
+  tokens[i].ty    = TK_EOF;
+  tokens[i].input = p;
+}
 
 int main(int argc, char **argv) {
   if (argc != 2) {
@@ -7,31 +70,39 @@ int main(int argc, char **argv) {
     return 1;
   }
 
-  char *p = argv[1]; 
+  tokenize(argv[1]);
 
+  // pre part of assembly
   printf(".intel_syntax noprefix\n");
   printf(".global main\n");
-  printf("\n");
-  printf("main:\n");
-  printf("  mov rax, %ld\n", strtol(p, &p, 10));
+  printf("main:\n\n");
 
-  while (*p) {
-    if (*p == '+') {
-      p++;
-      printf("  add rax, %ld\n", strtol(p, &p, 10));
+  if (tokens[0].ty != TK_NUM) 
+    error("first term is not number");
+  else 
+    printf("  mov rax, %d\n", tokens[0].val);
+
+  int i = 1;
+  while (tokens[i].ty != TK_EOF) {
+    if (tokens[i].ty == '+') {
+      i++;
+      if (tokens[i].ty != TK_NUM)
+        error("no expected word: %s\n", tokens[i].input);
+      printf(" add rax, %d\n", tokens[i].val);
+      i++;
+      continue;
+    }
+    if (tokens[i].ty == '-') {
+      i++;
+      if (tokens[i].ty != TK_NUM)
+        error("no expected word: %s\n", tokens[i].input);
+      printf(" sub rax, %d\n", tokens[i].val);
+      i++;
       continue;
     }
 
-    if (*p == '-') {
-      p++;
-      printf("  sub rax, %ld\n", strtol(p, &p, 10));
-      continue;
-    }
-
-    fprintf(stderr, "no expect word: '%c'\n", *p);
-    return 1;
+    error("no expected word: %s\n", tokens[i].input);
   }
-
   printf("  ret\n");
   return 0;
 }
