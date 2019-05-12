3c31c3085ef74e6d8cefbbf7398e18663d4efa46
kida <tomo426.sakura1281m@gmail.com>
2019-05-13 02:47:24 +0900

    可変長ベクタとその構造のユニットテスト追加

--- 9cc.c
+++ 9cc.c
@@ -27,12 +27,22 @@ typedef struct Node {
   int val;          // use if ty is 'ND_NUM'
 } Node;
 
+typedef struct {
+  void **data;
+  int capacity;
+  int len;
+} Vector;
+
 // token array tokenized save this array.
 // define that more 100 tokens don't come
 Token tokens[100];
 // position of tokens now
 int pos = 0;
  
+Vector *new_vector();
+void vec_push(Vector *vec, void *elem);
+int expect(int line, int expected, int actual);
+void runtest();
 void error(char *fmt, ...);
 void tokenize(char *p);
 Node *new_node(int ty, Node *lhs, Node *rhs);
@@ -43,6 +53,46 @@ Node *mul();
 Node *unary();
 Node *term();
 void gen(Node *node);
+
+Vector *new_vector() {
+  Vector *vec = malloc(sizeof(Vector));
+  vec->data = malloc(sizeof(void *) * 16);
+  vec->capacity = 16;
+  vec->len = 0;
+  return vec;
+}
+
+void vec_push(Vector *vec, void *elem) {
+  if (vec->capacity == vec->len) {
+    vec->capacity *= 2;
+    vec->data = realloc(vec->data, sizeof(void *) * vec->capacity);
+  }
+  vec->data[vec->len++] = elem;
+}
+
+int expect(int line, int expected, int actual) {
+  if (expected == actual) 
+    return;
+  fprintf(stderr, "%d: %d expected, but got %d\n",
+      line, expected, actual);
+  exit(1);
+}
+
+void runtest() {
+  Vector *vec = new_vector();
+  expect(__LINE__, 0, vec->len);
+
+  int i;
+  for (i = 0; i < 100; i++)
+    vec_push(vec, (void *)i);
+
+  expect(__LINE__, 100, vec->len);
+  expect(__LINE__, 0, (long)vec->data[0]);
+  expect(__LINE__, 50, (long)vec->data[50]);
+  expect(__LINE__, 99, (long)vec->data[99]);
+
+  printf("OK\n");
+}
 
 // it report error
 // it have same arguments to 'printf'
@@ -199,6 +249,11 @@ int main(int argc, char **argv) {
   if (argc != 2) {
     fprintf(stderr, "num of arguments is not corrent\n");
     return 1;
+  }
+
+  if (strcmp(argv[1], "-test")==0) {
+    runtest();
+    return 0;
   }
 
   // tokenize and parse
